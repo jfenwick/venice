f21b395317025ba8ce7f13e72c5e64a77eb53636
Jacob Fenwick <jacob.fenwick@gmail.com>
2014-05-08 19:39:20 -0400

    added working matdan example for unwrapping complex dictionary nesting

--- coreFunctionality/cascade.py
+++ coreFunctionality/cascade.py
@@ -38,6 +38,12 @@ class ArduinoDriver:
 
 		self.updateEmitters(emitters)
 
+	def unwrapEmitters(self, wrapped_emitters):
+		emitters = []
+		for i,emitter in enumerate(wrapped_emitters.itervalues()):
+			emitters.append(emitter)
+		self.updateEmitters(emitters)
+
 	def updateEmitters(self, emitters):
 		servoArduinoIndex = 0
 		bulbArduinoIndex = 1
@@ -55,7 +61,8 @@ class ArduinoDriver:
 				angle = 135
 			elif angle < 45:
 				angle = 45
-			self.data_store[e[servoArduinoIndex]][e[servoPinIndex]] = angle
+			tmp = self.data_store[int(e[servoArduinoIndex])]
+			tmp[int(e[servoPinIndex])] = angle
 
 	def updateArduinos(self):
 		# if enough time has elapsed since the last update, update arduinos
@@ -67,7 +74,6 @@ class ArduinoDriver:
 				for data in sorted_data:
 					serial_data = serial_data + str(data[1]).zfill(3)
 				serial_data = serial_data + "\0"
-				print serial_data
 				device.port.write(serial_data)
 
 
@@ -110,24 +116,20 @@ if __name__ == "__main__":
 	paths = []
 	paths.append('/dev/tty.usbmodem14141')
 
-	num_emitters = 35
+	num_emitters = 3
 	emitters = []
 
-	matdan = False
+	matdan = True
 
-	if matdan:
-		emitters = {(1, 3): ['0', '2', '9', '9', 1, 0], (2, 8): ['1', '3', '8', '8', 0, 0], (0, 7): ['0', '2', '7', '7', 0, 0], (2, 6): ['1', '3', '6', '6', 0, 0.0], (1, 6): ['0', '2', '12', '12', 0, 0], (0, 10): ['0', '2', '10', '10', 0, 0], (0, 3): ['0', '2', '3', '3', 1, 0], (2, 5): ['1', '3', '5', '5', 0, 20.504543450785103], (1, 2): ['0', '2', '8', '8', 0, 0], (2, 9): ['1', '3', '9', '9', 0, 0], (2, 0): ['1', '3', '0', '0', 0, 0], (1, 5): ['0', '2', '11', '11', 0, 0], (0, 4): ['0', '2', '4', '4', 1, 0], (1, 10): ['0', '2', '16', '16', 0, 0], (1, 1): ['0', '2', '7', '7', 0, 0], (2, 7): ['1', '3', '7', '7', 0, 0], (0, 0): ['0', '2', '0', '0', 0, 0], (2, 2): ['1', '3', '2', '2', 0, 20.504543450785103], (1, 4): ['0', '2', '10', '10', 1, 0], (2, 10): ['1', '3', '10', '10', 0, 0], (2, 1): ['1', '3', '1', '1', 0, 0.0], (0, 5): ['0', '2', '5', '5', 0, 0], (1, 9): ['0', '2', '15', '15', 0, 0], (1, 0): ['0', '2', '6', '6', 0, 0], (0, 8): ['0', '2', '8', '8', 0, 0], (0, 1): ['0', '2', '1', '1', 0, 0], (2, 4): ['1', '3', '4', '4', 2, 41.0090869015702], (0, 6): ['0', '2', '6', '6', 0, 0], (1, 8): ['0', '2', '14', '14', 0, 0], (1, 7): ['0', '2', '13', '13', 0, 0], (0, 9): ['0', '2', '9', '9', 0, 0], (2, 3): ['1', '3', '3', '3', 2, 41.0090869015702], (0, 2): ['0', '2', '2', '2', 0, 0]}
-
-	else:
-		for i in range(0, num_emitters):
-			emitters.append([0, 1, i+2, i+2, 0, 45])
+	for i in range(0, num_emitters):
+		emitters.append([0, 1, i+2, i+2, 0, 45])
 
 	driver = ArduinoDriver(emitters, paths)
 	try:
 		driver.open_ports()
 
 		if matdan:
-			pass
+			emitters = {(1, 3): ['0', '1', '2', '2', 1, 45], (2, 8): ['0', '1', '3', '3', 0, -45], (3, 8): ['0', '1', '4', '4', 0, -45]}
 		else:
 			for i in range(0, num_emitters):
 				if i % 2 == 0:
@@ -139,7 +141,10 @@ if __name__ == "__main__":
 		print 'entering loop'
 
 		while True:
-			driver.updateEmitters(emitters)
+			if matdan:
+				driver.unwrapEmitters(emitters)
+			else:
+				driver.updateEmitters(emitters)
 			driver.updateArduinos()
 	finally:
 		driver.close_ports()
